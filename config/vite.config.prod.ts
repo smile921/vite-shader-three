5ab09ec60fc380cd7c34f398094b95bb36ced963
smile921 <smile921@smile.org>
2024-05-16 10:49:42 +0800

    init project

---
+++ config/vite.config.prod.ts
@@ -0,0 +1,28 @@
+import { mergeConfig } from 'vite'
+import configArcoResolverPlugin from './plugin/arcoResolver'
+import configCompressPlugin from './plugin/compress'
+import configVisualizerPlugin from './plugin/visualizer'
+import baseConig from './vite.config.base'
+
+export default mergeConfig(
+  {
+    mode: 'production',
+    plugins: [configCompressPlugin('gzip'), configVisualizerPlugin(), configArcoResolverPlugin()],
+    build: {
+      chunkSizeWarningLimit: 20480,
+      reportCompressedSize: false,
+      rollupOptions: {
+        onwarn: () => {},
+        output: {
+          chunkFileNames: 'static/js/[name]-[hash].js',
+          entryFileNames: 'static/js/[name]-[hash].js',
+          assetFileNames: 'static/[ext]/[name]-[hash].[ext]',
+        },
+      },
+      minify: 'esbuild',
+      target: 'es2015',
+      sourcemap: false,
+    },
+  },
+  baseConig
+)
