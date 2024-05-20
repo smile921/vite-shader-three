8cd064fccfd115b06142e24903e67279c2af6857
smile921 <smile921@smile.org>
2024-05-20 16:38:58 +0800

    cert

--- config/vite.config.dev.ts
+++ config/vite.config.dev.ts
@@ -1,6 +1,7 @@
 import { mergeConfig } from 'vite'
 import eslint from 'vite-plugin-eslint'
 import baseConig from './vite.config.base'
+const fs = require('fs')
 
 export default mergeConfig(
   {
@@ -8,6 +9,11 @@ export default mergeConfig(
     server: {
       open: true,
       host: '0.0.0.0',
+      port: 443,
+      https: {
+        key: fs.readFileSync('./.cert/key.pem'),
+        cert: fs.readFileSync('./.cert/cert.pem'),
+      },
       fs: {
         strict: true,
       },
