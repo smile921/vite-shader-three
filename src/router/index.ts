abbf6c775bbde2f89351b0e7e203ba6b6868920f
smile921 <smile921@smile.org>
2024-05-27 22:02:10 +0800

    fix login with mock data to work around

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,16 @@
-import { createRouter, createWebHashHistory, LocationQueryRaw } from 'vue-router'
-import NProgress from 'nprogress' // progress bar
-import 'nprogress/nprogress.css'
+import { createRouter, createWebHashHistory, LocationQueryRaw } from 'vue-router';
+import NProgress from 'nprogress'; // progress bar
+import 'nprogress/nprogress.css';
 
-import usePermission from '@/hooks/permission'
-import { useUserStore } from '@/store'
-import PageLayout from '@/layout/page-layout.vue'
+import usePermission from '@/hooks/permission';
+import { useUserStore } from '@/store';
+import PageLayout from '@/layout/page-layout.vue';
 // import { isLogin } from '@/utils/auth'
-import appRoutes from './modules'
-import { toolRoutes } from '../views/index'
+import appRoutes from './modules';
+import { toolRoutes } from '../views/index';
 
-console.log(toolRoutes, PageLayout)
-NProgress.configure({ showSpinner: false }) // NProgress Configuration
+console.log(toolRoutes, PageLayout);
+NProgress.configure({ showSpinner: false }); // NProgress Configuration
 
 const router = createRouter({
   history: createWebHashHistory(''),
@@ -33,30 +33,30 @@ const router = createRouter({
     },
   ],
   scrollBehavior() {
-    return { top: 0 }
+    return { top: 0 };
   },
-})
+});
 
 router.beforeEach(async (to, from, next) => {
-  NProgress.start()
-  const userStore = useUserStore()
+  NProgress.start();
+  const userStore = useUserStore();
   async function crossroads() {
-    const Permission = usePermission()
-    if (Permission.accessRouter(to)) await next()
+    const Permission = usePermission();
+    if (Permission.accessRouter(to)) await next();
     else {
       const destination = Permission.findFirstPermissionRoute(appRoutes, userStore.role) || {
         name: 'notFound',
-      }
-      await next(destination)
+      };
+      await next(destination);
     }
-    NProgress.done()
+    NProgress.done();
   }
   if (userStore.role) {
-    crossroads()
+    crossroads();
   } else {
     try {
-      await userStore.info()
-      crossroads()
+      await userStore.info();
+      crossroads();
     } catch (error) {
       next({
         name: 'home',
@@ -64,10 +64,10 @@ router.beforeEach(async (to, from, next) => {
           redirect: to.name,
           ...to.query,
         } as LocationQueryRaw,
-      })
-      NProgress.done()
+      });
+      NProgress.done();
     }
   }
-})
+});
 
-export default router
+export default router;
--- src/store/modules/user/index.ts
+++ src/store/modules/user/index.ts
@@ -1,7 +1,7 @@
-import { defineStore } from 'pinia'
-import { login as userLogin, logout as userLogout, getUserInfo, LoginData } from '@/api/user'
-import { setToken, clearToken } from '@/utils/auth'
-import { UserState } from './types'
+import { defineStore } from 'pinia';
+import { login as userLogin, logout as userLogout, getUserInfo, LoginData } from '@/api/user';
+import { setToken, clearToken } from '@/utils/auth';
+import { UserState } from './types';
 
 const useUserStore = defineStore('user', {
   state: (): UserState => ({
@@ -25,53 +25,72 @@ const useUserStore = defineStore('user', {
 
   getters: {
     userInfo(state: UserState): UserState {
-      return { ...state }
+      return { ...state };
     },
   },
 
   actions: {
     switchRoles() {
       return new Promise((resolve) => {
-        this.role = this.role === 'user' ? 'admin' : 'user'
-        resolve(this.role)
-      })
+        this.role = this.role === 'user' ? 'admin' : 'user';
+        resolve(this.role);
+      });
     },
     // Set user's information
     setInfo(partial: Partial<UserState>) {
-      this.$patch(partial)
+      this.$patch(partial);
     },
 
     // Reset user's information
     resetInfo() {
-      this.$reset()
+      this.$reset();
     },
 
     // Get user's information
     async info() {
-      const res = await getUserInfo()
-
-      this.setInfo(res.data)
+      // const res = await getUserInfo()
+      const userInfo = {
+        name: '王立群',
+        avatar: 'https://i.gtimg.cn/club/item/face/img/2/15922_100.gif',
+        email: 'wangliqun@email.com',
+        job: 'frontend',
+        jobName: '前端艺术家',
+        organization: 'Frontend',
+        organizationName: '前端',
+        location: 'beijing',
+        locationName: '北京',
+        introduction: '人潇洒，性温存',
+        personalWebsite: 'https://www.arco.design',
+        phone: '150****0000',
+        registrationDate: '2013-05-10 12:10:00',
+        accountId: '15012312300',
+        certification: 1,
+        role: 'admin',
+      };
+      // this.setInfo(res.data);
+      this.setInfo(userInfo);
     },
 
     // Login
     async login(loginForm: LoginData) {
       try {
-        const res = await userLogin(loginForm)
-        setToken(res.data.token)
+        // const res = await userLogin(loginForm);
+        // setToken(res.data.token);
+        setToken('user-mock-token');
       } catch (err) {
-        clearToken()
-        throw err
+        clearToken();
+        throw err;
       }
     },
 
     // Logout
     async logout() {
-      await userLogout()
+      await userLogout();
 
-      this.resetInfo()
-      clearToken()
+      this.resetInfo();
+      clearToken();
     },
   },
-})
+});
 
-export default useUserStore
+export default useUserStore;
